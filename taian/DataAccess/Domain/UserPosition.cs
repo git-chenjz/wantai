566b93cd2ae6f52b9fa317f4271cc8d87d19643b
git-chenjz <3234305260@qq.com>
2016-03-04 13:26:28 +0800

    change
    change

---
+++ taian/DataAccess/Domain/UserPosition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Domain
+{
+    /// <summary>
+    /// 用户定位
+    /// </summary>
+    public class UserPosition
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public string WechatUserID { get; set; }
+        /// <summary>
+        /// 经度
+        /// </summary>
+        public double Longitude { get; set; }
+        /// <summary>
+        /// 纬度
+        /// </summary>
+        public double Latitude { get; set; }
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public DateTime UpdateTime { get; set; }
+    }
+}
---
+++ taian/WanTaiWeb/Areas/Web/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using DataService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WanTaiWeb.Infrastructure;
+
+namespace WanTaiWeb.Areas.Web.Controllers
+{
+    public class UserController : WechatLoginController
+    {
+        #region 字段
+
+        private IADService _adService;
+        private IPageService _pageService;
+        private IVideoService _videoService;
+        private ILBSService _lbsService;
+        private ICycleTableService _cycleTableService;
+
+        #endregion
+
+        #region 构造
+
+        public UserController(IADService adService, IPageService pageService, IVideoService videoService, ILBSService lbsService, ICycleTableService cycleTableService)
+        {
+            _adService = adService;
+            _pageService = pageService;
+            _videoService = videoService;
+            _lbsService = lbsService;
+            _cycleTableService = cycleTableService;
+        }
+
+        #endregion
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
